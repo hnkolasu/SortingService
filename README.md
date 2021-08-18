7ab7fd8d5c8e38733034204a8a47368924bfd261
Nicolas Siemens Quiering <nicolasquiering@gmail.com>
2021-08-18 00:41:39 -0300

    second commit
    commented, sortingServiceException working

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # sortingService
  
+ This is a service that sorts the books it receives by any parameter, in ascending or descending order.
+ it works by receiving the JSON by the POST request, reading from the JSON which parameter the books must be sort by, and in which order, it then sorts by the primary parameter. In the case of a tie, it goes to a secondary parameter if there is one, and so on. It then responds with a JSON with all the books in the requested order.
+
+ The JSON must be passed by the request body and the formatting must be as follows:
+
+ {
+     "books": [
+         {
+             "id":"01",
+             "title":"title 1",
+             "author":"author 1",
+             "edition":"edition 1"
+         },
+         {
+             "id":"02",
+             "title":"title 2",
+             "author":"author 2",
+             "edition":"edition 2"
+         }
+     ],
+     "options":
+     {
+         "filters":["edition", "author", "title"]
+         "orders":["desc", "desc", "asc"]
+     }
+ }
+
+A few notes:
+The "books" parameter holds an array of objects where each object is a book on the list. It can hold as many books and each book can have as many filters as you'd like, as long as it follows this pattern.
+
+The "options" parameter holds the filters, and the respective orders of the filters.
+
+The information must always be sent as a string.
+
+
+How to run the project locally:
+
+After cloning the project, open the terminal and execute the command "npm install"
+After that, run the project by using the command "npm start"
+While running, send a POST request to localhost:8000/books with the correctly formatted JSON
+
+projeto, como funciona, breve descricao
+como é o formato do json
+
+como rodar localmente
+-clonar o projeto e executar o "npm install"
+-rodar o projeto com "npm start"
+-enviar um post para localhost:8000/livros com o json formatado corretamente
--- index.js
+++ index.js
@@ -14,40 +14,66 @@ app.listen(HTTP_PORT, () => {
 
 
 
-app.post("/ola", (req, res, next) => {
+function sortingServiceException(message) { // function to send an error when needed
+    const error = new Error(message);
+    return error;
+  }
 
-    let livros = req.body.livros
+
+
+app.post("/books", (req, res, next) => {
+
+    if(req.body.options.filters.length || req.body.options.orders.length == 0){  // Throws an error if the filters or orders are empty
+
+        throw new sortingServiceException('"options" cannot be empty');
+    
+    }
+
+
+    // Sets up books and options variables
+    let books = req.body.books
     let options = req.body.options
 
-    function comparator(a,b) {
-        var i = -1,
-        length = options.props.length,
+
+    //comparator function takes "a" and "b" as variables, as these variables are needed for the sort() function, where the comparator function is called
+    function comparator(a,b) { 
+        var i = 0,
+        length = options.filters.length,
         ordersLength = options.orders.length;
-        props = options.props;
+        filters = options.filters;
         orders = options.orders;
         
-        while (++i < length) {
-            var result = a[props[i]].localeCompare(b[props[i]]);
-            if (result) {
-                if (i >= ordersLength) {
+
+        //the while runs through for each parameter
+
+        while (i < length) {
+
+            var result = a[filters[i]].localeCompare(b[filters[i]]);         //a and b are each books which here are compared, and return 1, 0, or -1, if "a" is first, equal or after "b" respectively 
+            
+            if (result) {                                                    // If the result is 0, which means they are equal, it doesnt enter this if, and the next parameter is tested
+
+                if (i >= ordersLength) {                                     //returns result to sort if all filters are tested
                     return result;
                 }
+
                 var order = orders[i];
-                return result * (order == 'desc' ? -1 : 1);
+                return result * (order == 'desc' ? -1 : 1);                  // checks order, if its descending sends -1, else 1
             }
+
+        ++i
         }
     }
 
-    livros.sort(comparator);
-    res.json(livros);
+    books.sort(comparator);
+    res.json(books);
 });
 
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
 
-app.get("/ola", (req, res, next) => {
-    res.json({"message":"Ao invés de um GET, use um POST com os parametros certos no JSON para ordenar os livros!"})
+app.get("/books", (req, res, next) => { // since GET request are the most common, this is a good way to signal if the requester is making this mistake
+    res.json({"message":"Instead of sending a GET request, send a POST request to sort the books!"})
 });
 
 
